92b747171e4e223e00e8e2db1cd9223b0af61220
Aman <aman@foody.com>
2025-01-17 16:33:40 +0530

    second commit for show message in case not fillup the input field

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let minus = document.getElementById("minus");
 let plus = document.getElementById("plus");
 let input = document.getElementById("input");
 let reset = document.getElementById("reset");
+let para = document.getElementById("para");
 
 let inputValue;
 let displayValue;
@@ -14,17 +15,29 @@ reset.addEventListener("click", () => {
 
 // minus button function
 minus.addEventListener("click", () => {
-  inputValue = Number.parseInt(input.value);
-  displayValue = Number.parseInt(display.innerText);
-  displayValue = displayValue - inputValue;
-  display.innerText = displayValue;
+  inputValue = input.value;
+  if (inputValue == "") {
+    para.style.visibility = "visible";
+    return;
+  } else {
+    para.style.visibility = "hidden";
+    inputValue = Number.parseInt(input.value);
+    displayValue = Number.parseInt(display.innerText);
+    displayValue = displayValue - inputValue;
+    display.innerText = displayValue;
+  }
 });
 
 // plus button function
 plus.addEventListener("click", () => {
+  inputValue = input.value;
+  if (inputValue == "") {
+    para.style.visibility = "visible";
+    return;
+  }
+  para.style.visibility = "hidden";
   inputValue = Number.parseInt(input.value);
   displayValue = Number.parseInt(display.innerText);
   displayValue = displayValue + inputValue;
-  console.log(displayValue);
   display.innerText = displayValue;
 });
